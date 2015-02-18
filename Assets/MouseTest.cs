27400e3eeda749b0863b61e8faf9365fb6e20a2e
M. Haider <mrhaider@gmail.com>
2015-02-18 07:17:10 -0500

    second commit

---
+++ Assets/MouseTest.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseTest : MonoBehaviour
+{
+    RaycastHit hit;
+    public Transform mousetar;
+    public Vector3 mousepos;
+    #region
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    MoveObject();
+
+	    /*        //mousepos = Input.get
+                mousepos = Input.mousePosition;
+                mousepos = Camera.main.ScreenToWorldPoint(mousepos);
+                Vector3 mousetarpos;
+                mousetarpos.x = mousepos.x;
+                mousetarpos.z = mousepos.y;
+                mousetarpos.y = 1.1f;
+	   
+                //mousepos.y = 1.1f;
+                mousetar.position = mousetarpos;*/
+	}
+
+    private void MoveObject()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            mousetar.position = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+        }
+    }
+
+
+    #endregion
+}
---
+++ Assets/Script/Tools/XRayMouse.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class XRayMouse : MonoBehaviour
+{
+    #region class variables and properties
+    [SerializeField] private float radius;
+    // Use this for initialization
+    #endregion
+
+    #region unity funcs
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    LookInside();
+        renderer.material.SetFloat("_Radius", radius);
+	}
+
+    private void LookInside()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            renderer.material.SetVector("_ObjPos", new Vector4(hit.point.x,hit.point.y,hit.point.z,0));
+
+        }
+
+    }
+    #endregion
+}
